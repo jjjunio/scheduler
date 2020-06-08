c2b51d5142b580e226cdf3c94de34c5891185362
S0l0kr3w <44209188+S0l0kr3w@users.noreply.github.com>
2020-06-08 18:04:48 -0400

    configured storybook and created Button, DayList, DayListItem components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,8 @@
 import { configure } from "@storybook/react";
 
-function loadStories() {
-  require("../stories");
+function loadStories() { [
+  require("../stories/index.js")
+]
 }
 
 configure(loadStories, module);
